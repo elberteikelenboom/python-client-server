916bb155370842f63c8c1c403db0a902c5d82aef
Elbert Eikelenboom <elbert.eikelenboom@gmail.com>
2019-12-29 12:55:18 +0100

    - Initial code commit.

---
+++ TestServer.py
@@ -0,0 +1,18 @@
+from Server import Server
+
+
+def handler(connection):
+    connection.sendall('Hello\n')
+    data = connection.receive()
+    print(data)
+    return 0
+
+
+def main():
+    # server = Server.create('tcp', handler, '127.0.0.1', 8080)
+    server = Server.create('unix', handler, '/home/elbert/server', max_connections=2)
+    server.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
